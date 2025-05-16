2bbccf2ed297ee6ff4261447a5fc4f03e0160567
saisab29 <saisabk29@gmail.com>
2025-05-16 22:59:51 +0545

    Update Landing Page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton, SignUpButton } from "@clerk/clerk-react";
+import { ClerkLoaded, ClerkLoading, SignedIn } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>This is main page</div>;
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:2-[424px] lg:h-[424px] mb-8: lg:mb-0">
+        <Image alt="hero" fill src="/hero.svg"></Image>
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 mx-w-[480px] text-center">
+          Learn, practice, and master enw language with Lingo.
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton>
+                <Button size="lg" variant="secondary" className="w-full">
+                  GetStarted
+                </Button>
+              </SignUpButton>
+              <SignInButton>
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
