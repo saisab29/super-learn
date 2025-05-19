bf2840c20d18ee5d92ee4232849bc1daa106c6f5
saisab29 <saisabk29@gmail.com>
2025-05-19 21:42:41 +0545

    Update footer

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
-    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-100 p-2">
-      Footer
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/nodejs.svg"
+            alt="Croatian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Node
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/rust.svg"
+            alt="Spanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Rust
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/python.svg"
+            alt="French"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Python
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/nodejs.svg"
+            alt="Italian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Node
+        </Button>
+        <Button size="lg" variant="ghost">
+          <Image
+            src="/rust.svg"
+            alt="Japanese"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Rust
+        </Button>
+      </div>
     </footer>
   );
 };
