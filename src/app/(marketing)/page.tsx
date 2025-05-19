45c3a1ddfabdbb85ef60c0733f3a861c1e13d959
saisab29 <saisabk29@gmail.com>
2025-05-19 21:42:33 +0545

    Fix: Fix Clerk authentication from landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -10,25 +10,33 @@ import React from "react";
 const Page = () => {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
-      <div className="relative w-[240px] h-[240px] lg:2-[424px] lg:h-[424px] mb-8: lg:mb-0">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8: lg:mb-0">
         <Image alt="hero" fill src="/hero.svg"></Image>
       </div>
       <div className="flex flex-col items-center gap-y-8">
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 mx-w-[480px] text-center">
-          Learn, practice, and master enw language with Lingo.
+          Learn, practice, and master new language with SuperLearn.
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px]">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
           <ClerkLoaded>
             <SignedOut>
-              <SignUpButton>
+              <SignUpButton
+                mode="modal"
+                forceRedirectUrl="/"
+                fallbackRedirectUrl="/"
+              >
                 <Button size="lg" variant="secondary" className="w-full">
                   GetStarted
                 </Button>
               </SignUpButton>
-              <SignInButton>
+              <SignInButton
+                mode="modal"
+                forceRedirectUrl="/"
+                fallbackRedirectUrl="/"
+              >
                 <Button size="lg" variant="primaryOutline" className="w-full">
                   I already have an account
                 </Button>
