979bbd53339ab81f2d8f3db36a188cc3b639d132
saisab29 <saisabk29@gmail.com>
2025-05-21 22:05:56 +0545

    feat: Update user progress and active course

--- database/queries.ts
+++ database/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 export const getUserProgress = cache(async () => {
@@ -21,5 +21,13 @@ export const getUserProgress = cache(async () => {
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // units and lessons will also be here later
+  });
   return data;
 });
