2ceb691aa8f374ee3a2c52081628814e0117cb89
saisab29 <saisabk29@gmail.com>
2025-05-21 22:13:32 +0545

    feat(fe): Add sonner for toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const font = Nunito({
   variable: "--font-nunito",
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${font.className}antialiased`}>{children}</body>
+        <body className={`${font.className}antialiased`}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
