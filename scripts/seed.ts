7af939fcf405fcae6357609ddb4decaeb947b152
saisab29 <saisabk29@gmail.com>
2025-05-24 22:45:31 +0545

    Update initial seed values

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,6 +12,12 @@ const main = async () => {
     console.log("Seeding database");
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -28,6 +34,80 @@ const main = async () => {
         id: 3,
         title: "Rust",
         imageSrc: "/rust.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Python",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Variables",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Operators",
+      },
+      {
+        id: 3,
+        unitId: 1,
+        order: 3,
+        title: "Functions",
+      },
+      {
+        id: 4,
+        unitId: 1,
+        order: 4,
+        title: "List and Tuples",
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "double" type?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1, //Which one of these is the "python vairable"
+        text: "20",
+        imageSrc: "/int.svg",
+        correct: false,
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1, //Which one of these is the "python vairable"
+        text: "20.0",
+        imageSrc: "/float.svg",
+        correct: false,
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1, //Which one of these is the "python vairable"
+        text: "20.000",
+        imageSrc: "/double.svg",
+        correct: true,
+        audioSrc: "/es_man.mp3",
       },
     ]);
   } catch (error) {
