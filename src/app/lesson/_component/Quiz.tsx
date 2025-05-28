e46aae7aa51f3c28f543a579e820fdf7643eb615
saisab29 <saisabk29@gmail.com>
2025-05-28 21:23:11 +0545

    Update quiz component to display question from db

--- src/app/lesson/_component/Quiz.tsx
+++ src/app/lesson/_component/Quiz.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { challengeOptions, challenges } from "../../../../database/schema";
 import { Header } from "./Header";
+import { QuestionBubble } from "./QuestionBubble";
 
 type Props = {
   initialPercentage: number;
@@ -24,6 +25,21 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct answer"
+      : challenge.question;
   return (
     <>
       <Header
@@ -31,6 +47,20 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
