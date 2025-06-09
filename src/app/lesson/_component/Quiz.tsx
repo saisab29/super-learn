6324d6655d16b61a64594f460e939c8b1789639e
saisab29 <saisabk29@gmail.com>
2025-06-09 22:16:31 +0545

    feat(fe): Add sound effect for different scenarios

--- src/app/lesson/_component/Quiz.tsx
+++ src/app/lesson/_component/Quiz.tsx
@@ -9,6 +9,7 @@ import { Footer } from "./Footer";
 import { upsertChallengeProgress } from "../../../../actions/challenge-progress";
 import { toast } from "sonner";
 import { reduceHearts } from "../../../../actions/user-progress";
+import { useAudio } from "react-use";
 
 type Props = {
   initialPercentage: number;
@@ -28,6 +29,10 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [correctAudio, _c, correctControls] = useAudio({ src: "/correct.wav" });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: "/incorrect.wav",
+  });
   const [pending, startTransition] = useTransition();
 
   const [hearts, setHearts] = useState(initialHearts);
@@ -80,7 +85,7 @@ export const Quiz = ({
               console.error("Missing hearts");
               return;
             }
-
+            correctControls.play();
             setStatus("correct");
             setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -99,6 +104,8 @@ export const Quiz = ({
               console.error("Missing hearts");
               return;
             }
+
+            incorrectControls.play();
             setStatus("wrong");
 
             if (!response?.error) {
@@ -119,6 +126,8 @@ export const Quiz = ({
       : challenge.question;
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
